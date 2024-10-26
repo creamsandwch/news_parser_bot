96d07498cb87fdc09ea328b8c14ae54e0ee200d1
unknown <kreamsandwich@gmail.com>
2024-10-26 20:26:23 +0500

    fix: ротация логов, отчёт по источникам парсинга

--- bot_app/bot.py
+++ bot_app/bot.py
@@ -1,6 +1,7 @@
 import os
 
 import telebot
+import threading
 import time
 from typing import List
 
@@ -23,21 +24,34 @@ NEWS_PARSERS: List[AbstractParser] = [
 
 @bot.message_handler(content_types=['text'])
 def commands(message):
-    if message.text == "старт":
-        while True:
-            for parser in NEWS_PARSERS:
-                news_object: dict = parser.get_last_news_object()
-                if news_object is not None:
-                    bot.send_message(
-                        chat_id=CHANNEL_ID,
-                        text='{}\n{}'.format(
-                            news_object.get('title'),
-                            news_object.get('link')
+    try:
+        if message.text == "старт":
+            while True:
+                news_parsers_info = '\n'.join(
+                    [f'{x.__name__}: {x.URL}' for x in NEWS_PARSERS]
+                )
+                bot.send_message(
+                    message.from_user.id,
+                    text=f'Парсинг источников: {news_parsers_info}'
+                )
+                for parser in NEWS_PARSERS:
+                    news_object: dict = parser.get_last_news_object()
+                    if news_object is not None:
+                        bot.send_message(
+                            chat_id=CHANNEL_ID,
+                            text='{}\n{}'.format(
+                                news_object.get('title'),
+                                news_object.get('link')
+                            )
                         )
-                    )
-                time.sleep(1800)
-    else:
+                    time.sleep(1800)
+        else:
+            bot.send_message(
+                message.from_user.id,
+                "Я тебя не понимаю. Напиши 'старт'"
+            )
+    except Exception as e:
         bot.send_message(
             message.from_user.id,
-            "Я тебя не понимаю. Напиши 'старт'"
+            'Ошибка: {}'.format(str(e))
         )
---
+++ bot_app/log.py
@@ -0,0 +1,29 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Set up logging configuration
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)  # Set root logger to the lowest level
+
+# Create a rotating file handler for INFO level logging
+rotating_handler = RotatingFileHandler(
+    "parser.log",  # Log file name
+    maxBytes=5 * 1024 * 1024,  # Maximum log file size in bytes (5MB in this example)
+    backupCount=5  # Number of backup files to keep
+)
+rotating_handler.setLevel(logging.INFO)  # Set file handler level to INFO
+
+# Create a console handler for DEBUG level logging
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)  # Set console handler level to DEBUG
+
+# Define the log format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# Attach the formatter to both handlers
+rotating_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add both handlers to the logger
+logger.addHandler(rotating_handler)
+logger.addHandler(console_handler)
